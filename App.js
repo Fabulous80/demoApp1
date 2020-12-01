aaac90d914b321f2aac448801a3ab3975376c4dc
Marcus Lim <marcz68@gmail.com>
2020-12-01 12:17:17 +0800

    add counter

--- App.js
+++ App.js
@@ -1,14 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  function buttonPressed() {
+    console.log("Button was pressed!");
+    setCount(count + 1);
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{count}</Text>
+
+      <Button title="Press Me!" onPress={buttonPressed}></Button>
+
       <StatusBar style="auto" />
     </View>
   );
+}
+
+
+const App = () => {
+  
+  let [count, setCount] = useState(0)
+  
+  function increment(){
+    setCount(count +1)
+  }
+  
+  function decrement(){
+
+    if (count>0){
+       setCount(count -1)
+    }
+   
+    
+  }
+
+  function reset(){
+    setCount(0)
+  }
+  
+  return (
+    
+    <div className="App">
+    
+     <h1>{count}</h1>
+    
+      <button onClick={increment}> Click to increment!</button>
+      <button onClick={decrement}> Click to decrement!</button>
+      <button onClick={reset}> Click to reset!</button>
+    </div>
+    
+    
+    );
 }
 
 const styles = StyleSheet.create({
